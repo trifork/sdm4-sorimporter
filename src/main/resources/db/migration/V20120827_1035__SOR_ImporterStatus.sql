c2855a82a4a46a97761a58036f42aa880e54ffd5
Jakob Færch <jrf@trifork.com>
2012-10-28 20:32:34 +0100

    NSPSUPPORT-103: Databaseskema kan nu køres på eksisterende sdm3-skema uden runtime-fejl

--- src/main/resources/db/migration/V20120827_1035__SOR_ImporterStatus.sql
+++ src/main/resources/db/migration/V20120827_1035__SOR_ImporterStatus.sql
@@ -1,4 +1,4 @@
-CREATE TABLE sorimporterImportStatus (
+ CREATE TABLE IF NOT EXISTS sorimporterImportStatus (
     Id BIGINT(15) AUTO_INCREMENT NOT NULL PRIMARY KEY,
     StartTime DATETIME NOT NULL,
     EndTime DATETIME,
